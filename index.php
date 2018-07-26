db86d1bea476c77eca8c812f68ce55f16496dc5c
QuentinDll <quentin.delplace13@gmail.com>
2018-07-26 16:29:05 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $isOk = true;
+
+      if ($isOk){
+        echo "C'est ok !!!";
+
+      }else{
+        echo "C'est pas bon !!!";
+      }
+       ?>
+    </p>
+  </body>
+</html>
